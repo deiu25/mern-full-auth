86c06eb4d08e994b4aa752aa417603cb61511f1e
deiu25 <deiu25@gmail.com>
2023-08-30 14:35:07 +0300

    Leyout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Home } from './pages/home/Home';
+import { Leyout } from './components/layout/Leyaout';
 
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Leyout>
+          <Home />
+        </Leyout>} />
       </Routes>
     </BrowserRouter>
     </>
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import './Home.css'
-import { Header } from '../../components/header/Header'
-import Footer from '../../components/footer/Footer'
 import loginImg from '../../assets/login.svg'
 
 export const Home = () => {
   return (
     <>
-    <Header />
     <section className='container hero'>
       <div className='hero-text'>
         <h2>Ultimate MERN Stack Authentication System</h2>
         <p>Learn to create a comprehensive authentication system using React, Node, Express, and MongoDB.</p>
-        <p>Gain the skills to incorporate user registration, login, password reset, social login, user permissions, email notifications, and more.</p>
+        <p>Develop the necessary skills to implement user registration, login, password reset, social login, user permissions, email notifications, and additional features.</p>
+        <p>Take the first step!</p>
+        <p>Discover more.</p>
         <div className='hero-buttons --flex-start'>
           <button className='--btn --btn-danger'>Get Started</button>
           <button className='--btn --btn-primary'>Learn More</button>
@@ -22,7 +21,6 @@ export const Home = () => {
           <img src={loginImg} alt='Login' />
         </div>
     </section>
-    <Footer />
     </>
   )
 }
