62a6e1d37ab8e6bedb09a19b1833a26de805e74b
deiu25 <deiu25@gmail.com>
2023-08-30 11:51:49 +0300

    Header, Footer, Home

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Header.css";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+
+const activeLink = ({ isActive }) => (isActive ? "active" : "");
+
+export const Header = () => {
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <header>
+      <nav>
+        <div className="logo" onClick={goHome}>
+          Syntax Seekers
+        </div>
+        <input type="checkbox" id="checkbox" />
+        <label htmlFor="checkbox" id="icon">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+            ></path>
+          </svg>
+        </label>
+        <ul>
+          <li>
+            <button className="--btn --btn-primary">
+              <Link to="/login">Login</Link>
+            </button>
+          </li>
+          <li>
+            <NavLink to="/profile" className={activeLink}>
+              Profile
+            </NavLink>
+          </li>
+          <li>
+            <button className="--btn --btn-secondary">Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import './Home.css'
+import { Header } from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+import loginImg from '../../assets/login.svg'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Header />
+    <section className='container hero'>
+      <div className='hero-text'>
+        <h2>Ultimate MERN Stack Authentication System</h2>
+        <p>Learn to create a comprehensive authentication system using React, Node, Express, and MongoDB.</p>
+        <p>Gain the skills to incorporate user registration, login, password reset, social login, user permissions, email notifications, and more.</p>
+        <div className='hero-buttons --flex-start'>
+          <button className='--btn --btn-danger'>Get Started</button>
+          <button className='--btn --btn-primary'>Learn More</button>
+        </div>
+        </div>
+        <div className='hero-image'>
+          <img src={loginImg} alt='Login' />
+        </div>
+    </section>
+    <Footer />
+    </>
   )
 }
