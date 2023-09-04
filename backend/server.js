5b3b06c29233719bc2d1759fd4253ac2bd882f7a
deiu25 <deiu25@gmail.com>
2023-09-04 11:48:27 +0300

    backend, server, folders, etc

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require ('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors(
+    ({
+        origin: ["http://localhost:30000", "https://mernd-full-auth.vercel.app"],
+        credentials: true
+    })
+))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    })
+    .catch(err => console.log(err))
