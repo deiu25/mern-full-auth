bffe81bb16a57c90fef3cd81c7e8bd4a9c4a146d
deiu25 <deiu25@gmail.com>
2023-09-13 15:50:42 +0300

    user stats

--- frontend/src/components/userStats/UserStats.js
+++ frontend/src/components/userStats/UserStats.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./UserStats.scss"
 import { InfoBox } from '../infoBox/InfoBox'
 import { FaUsers } from 'react-icons/fa';
 import { BiUserCheck, BiUserMinus, BiUserX } from 'react-icons/bi';
+import { useDispatch, useSelector } from 'react-redux';
+import { CALC_SUSPENDED_USER, CALC_VERIFIED_USER } from '../../redux/features/auth/authSlice';
 
 const icon1 = <FaUsers size={40} color="#fff" />;
 const icon2 = <BiUserCheck size={40} color="#fff" />;
@@ -10,6 +12,18 @@ const icon3 = <BiUserMinus size={40} color="#fff" />;
 const icon4 = <BiUserX size={40} color="#fff" />;
 
 export const UserStats = () => {
+
+  const dispatch = useDispatch();
+  const  { users, verifiedUsers, suspendedUsers } = useSelector(
+    (state) => state.auth
+  );
+  const univerifiedUsers = users.length - verifiedUsers;
+
+  useEffect(() => {
+    dispatch(CALC_VERIFIED_USER());
+    dispatch(CALC_SUSPENDED_USER());
+  }, [dispatch, users]);
+
   return (
     <div className='user-summary'>
         <h3 className='--mt'>User Stats</h3>
@@ -17,25 +31,25 @@ export const UserStats = () => {
         <InfoBox
                 icon={icon1}
                 title={"Total Users"}
-                count={"3"}
+                count={users.length}
                 bgColor="card1"
               />
                 <InfoBox
                 icon={icon2}
                 title={"Active Users"}
-                count={"2"}
+                count={verifiedUsers}
                 bgColor="card2"
                 />
                 <InfoBox
                 icon={icon3}
-                title={"Pending Users"}
-                count={"1"}
+                title={"Unverified Users"}
+                count={univerifiedUsers}
                 bgColor="card3"
                 />
                 <InfoBox
                 icon={icon4}
-                title={"Blocked Users"}
-                count={"0"}
+                title={"Suspended Users"}
+                count={suspendedUsers}
                 bgColor="card4"
                 />
         </div>
